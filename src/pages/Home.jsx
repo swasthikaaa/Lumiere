c681ceaec9ba0d303f9d62ff42a66bd251a188d5
swasthikaaa <swasthikalingaraj@example.com>
2025-12-18 14:07:38 +0530

    Fix white screen: Restore missing FeaturedCollection import in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import BrandStory from '../components/BrandStory';
 import JournalSection from '../components/JournalSection';
+import FeaturedCollection from '../components/FeaturedCollection';
 
 const Home = () => {
     return (
